104850adb1257684f30d24e0c795b71f32cd6a15
bdintu <4646555+bdintu@users.noreply.github.com>
2017-10-24 22:31:36 +0700

    Update mul.py

--- Lab05-Recursion/mul.py
+++ Lab05-Recursion/mul.py
@@ -1,22 +1,13 @@
-class mul:
-    def __init__(self, x, n):
-        self.x = x
-        self.n = n
+def mul(self, x, n):
+    if n == 0:
+        return 0
+    elif n > 0:
+        return x + self.mul(x, n-1)
+    elif n < 0:
+        return -x + self.mul(x, n+1)
 
-    def __str__(self):
-        return "%2d*%2d = %3d"%(self.x, self.n, self.mul(self.x, self.n))
-
-    def mul(self, x, n):
-        if n==0:
-            return 0
-        elif n > 0:
-            return x + self.mul(x, n-1)
-        elif n < 0:
-            return -x + self.mul(x, n+1)
-
-if __name__ == "__main__":
-    print(mul(2, 0))
-    print(mul(2, 5))
-    print(mul(2, -5))
-    print(mul(-2, 5))
-    print(mul(-2, -5))
+print(mul(2, 0))
+print(mul(2, 5))
+print(mul(2, -5))
+print(mul(-2, 5))
+print(mul(-2, -5))
